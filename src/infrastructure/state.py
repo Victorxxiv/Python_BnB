6edab24da6ea0091bb3c914cf755f08eee32aa57
Victor <victormwendwa804@gmail.com>
2024-08-16 13:13:56 +0300

    Setting up the requirements for Snake/pet bnb

---
+++ src/infrastructure/state.py
@@ -0,0 +1,10 @@
+activate_account = None
+
+
+def reload_account():
+    global activate_account
+    if not activate_account:
+        return
+
+    # TODO: pull owner account from the database.
+    pass
